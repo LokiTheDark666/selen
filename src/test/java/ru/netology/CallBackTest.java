3f6670b480b536793c95dfaef875b8a8e7aa5b8b
LokiTheDark666 <khcr.loki@mail.ru>
2022-07-31 18:16:05 +0300

    tests

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -1,10 +1,12 @@
 package ru.netology;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
 
@@ -18,24 +20,30 @@ public class CallBackTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
 
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
     }
 
     @Test
     void test() {
         driver.get("http://localhost:9999/");
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Вася Пупкин");
-        elements.get(1).sendKeys("+89996664545");
+        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Вася Пупкин-Пипкин");
+        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+89996664545");
+        // List<WebElement> elements = driver.findElements(By.className("input__control"));
+        //elements.get(0).sendKeys("Вася Пупкин-Пипкин");
+        //elements.get(1).sendKeys("+89996664545");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String text = driver.findElement(By.className("paragraph")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 

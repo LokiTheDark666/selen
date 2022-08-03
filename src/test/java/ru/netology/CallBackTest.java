ea7b22e103638a4c8c50cfbd8f351653ad4c2674
LokiTheDark666 <khcr.loki@mail.ru>
2022-08-03 15:54:47 +0300

    tests

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -36,14 +36,11 @@ public class CallBackTest {
     @Test
     void test() {
         driver.get("http://localhost:9999/");
-        driver.findElement(By.cssSelector("span[data-test-id='name'] input")).sendKeys("Вася Пупкин-Пипкин");
-        driver.findElement(By.cssSelector("span[data-test-id='phone'] input")).sendKeys("+89996664545");
-        // List<WebElement> elements = driver.findElements(By.className("input__control"));
-        //elements.get(0).sendKeys("Вася Пупкин-Пипкин");
-        //elements.get(1).sendKeys("+89996664545");
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Вася Пупкин-Пипкин");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+89996664545");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button__text")).click();
-        String text = driver.findElement(By.className("paragraph")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
